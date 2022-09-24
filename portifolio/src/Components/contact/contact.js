5f470061884dacade57215c4235e2ed6d615c902
Vanessa Mainara <mainaravanessa@gmail.com>
2022-09-24 17:21:24 -0300

    Update contact.js

--- portifolio/src/Components/contact/contact.js
+++ portifolio/src/Components/contact/contact.js
@@ -19,7 +19,7 @@ export default function Contact() {
                         </li>
                     </ul>
                 </div>
-                <form action="https://formsubmit.co/fecfce57d3e403900dad1688f2d9c317" method="POST" class="form-contact">
+                <form action="https://formsubmit.co/ mainaravanessa@gmail.com" method="POST" class="form-contact">
                     <input type="hidden" name="_next" value="https://vanessamainara-portifolio.vercel.app/" />
                     <input type="hidden" name="_template" value="box" />
                     <input type="hidden" name="_autoresponse" value="Recebi sua mensagem, muito obrigado por entrar em contato! Responderei o mais rápido possível..." />            
